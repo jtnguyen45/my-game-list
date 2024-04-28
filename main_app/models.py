4fd4215c9a2d81743b094ec2395323fbaabbfb7f
jtnguyen45 <jtnguyen45@gmail.com>
2024-04-27 22:16:02 -0700

    Add Game and UserGame models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Game(models.Model):
+    api_id = models.CharField()
+    name = models.CharField()
+    summary = models.TextField()
+
+class UserGame(models.Model):
+    RATING_OPTIONS = [(str(i), str(i)) for i in range(6)]
+    STATUS_OPTIONS = (
+        ('1', 'Not started'),
+        ('2', 'In progress'),
+        ('3', 'Finished'),
+        ('4', 'Dropped'),
+        ('5', 'Taking a break'),
+        ('6', 'Playing again!'),
+    )
+
+    api_id = models.CharField()
+    name = models.CharField()
+    summary = models.TextField()
+    rating = models.CharField(choices=RATING_OPTIONS, null=True, blank=True)
+    status = models.CharField(choices=STATUS_OPTIONS, default='1')
+
+    def __str__(self):
+        return self.get_status_display()
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'game_id': self.id})
