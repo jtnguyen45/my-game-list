78d64ce2e302a2f57e06c0357d48e2be1ea37acc
jtnguyen45 <jtnguyen45@gmail.com>
2024-04-27 22:17:33 -0700

    Add basic routing and api calls

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
-#from .models import Game
+from .models import UserGame
+import requests
+import environ
+env = environ.Env()
+env.read_env()
 
 def home(request):
-    return render(request, 'home.html')
+    games_data = get_game() #test connection to api, TODO: remove
+    return render(request, 'home.html', {'games_data': games_data})
 
 def about(request):
     return render(request, 'about.html')
+
+def games_index(request):
+    games = UserGame.objects.all()
+    return render(request, 'games/index.html', {
+        'games': games
+    })
+
+
+def get_game():
+    url = 'https://api.igdb.com/v4/games'
+    headers = {
+        'Client-ID': env('IGDB_CLIENT_ID'),
+        'Authorization': env('IGDB_ACCESS_TOKEN'),
+    }
+    body = 'fields *;'
+    response = requests.post(url, headers=headers, data=body)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
